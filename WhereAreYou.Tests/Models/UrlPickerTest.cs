c82dfe75c417123d0d8e1cc2c15ad9b65eda88f7
Eedrah <eedrah@users.noreply.github.com>
2015-03-08 22:46:04 +1300

    test url picker returns AAAA when given 1

---
+++ WhereAreYou.Tests/Models/UrlPickerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+using WhereAreYou.Models;
+
+namespace WhereAreYou.Tests.Models {
+    [TestFixture]
+    public class UrlPickerTest
+    {
+        [Test]
+        public void Returns_AAAA_When_Given_Id_0()
+        {
+            Assert.AreEqual("AAAA", UrlPicker.Encode(0));
+        }
+    }
+}
---
+++ WhereAreYou/Models/UrlPicker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhereAreYou.Models {
+    public class UrlPicker {
+        public static string Encode(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
