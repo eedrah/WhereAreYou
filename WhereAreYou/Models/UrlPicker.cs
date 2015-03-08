a6b4be70743a5244932d19e460f71e9a4c999126
Eedrah <eedrah@users.noreply.github.com>
2015-03-08 23:41:57 +1300

    pass tests

--- WhereAreYou/Models/UrlPicker.cs
+++ WhereAreYou/Models/UrlPicker.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace WhereAreYou.Models {
     public class UrlPicker {
-        public static string Encode(int i)
-        {
-            return "AAAA";
+        public const string Letters = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz123456789";
+        public const int UrlLength = 4;
+
+        public static string Encode(int id) {
+            int numberBase = Letters.Length;
+            if (id >= (int)Math.Pow(numberBase, UrlLength)) {
+                throw new OverflowException("Exhausted all URLs");
+            }
+
+            var sb = new StringBuilder();
+            for (int digit = UrlLength; digit > 0; digit--) {
+                int divisor = (int)Math.Pow(numberBase, digit - 1);
+                int index = id / divisor;
+                sb.Append(Letters[index]);
+                id -= index * divisor;
+            }
+            return sb.ToString();
         }
     }
 }
